0a055d75b781e595a5a5f341125de9d893556a1b
idocky <laskovec02@ukr.net>
2022-09-07 13:14:39 +0300

    added form corrected url

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -1,18 +1,4 @@
-"""myblog URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -20,7 +6,7 @@ from django.urls import path
 from post.views import main_page, add_article, ArticleListView, ArticleDetailView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', ArticleListView.as_view(), name='home'),
     path('new_article/', add_article, name='add_article'),
     path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detail')
